4594ca4e95da74671e78e471ad753fe1e888f4aa
Socheat <hongsocheat38@gmail.com>
2023-01-23 15:20:09 +0700

    add feature on index.js

--- DadJokeGenerator/index.js
+++ DadJokeGenerator/index.js
@@ -10,9 +10,21 @@ const options = {
 };
 const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
 async function getJoke() {
-  const response = await fetch(apiURL, options);
-  const data = await response.json();
-  jokeEl.innerText = data[0].joke;
+  try {
+    jokeEl.innerText = "Updating...";
+    btnEl.disable = true;
+    btnEl.innerText = "Loading...";
+    const response = await fetch(apiURL, options);
+    const data = await response.json();
+    jokeEl.innerText = data[0].joke;
+    btnEl.disable = false;
+    btnEl.innerText = "Tell me a joke";
+  } catch (error) {
+    jokeEl.innerText = "An error happened, try again later";
+    btnEl.disable = false;
+    btnEl.innerText = "Tell me a joke";
+    console.log(error);
+  }
 }
 
 btnEl.addEventListener("click", getJoke);
