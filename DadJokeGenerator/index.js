2beb5661cf72ffe4baf774c901569d849967a1c7
Socheat <hongsocheat38@gmail.com>
2023-01-23 14:52:52 +0700

    done 3 file

--- DadJokeGenerator/index.js
+++ DadJokeGenerator/index.js
@@ -1,1 +1,18 @@
+const btnEl = document.getElementById("btn");
+const jokeEl = document.getElementById("joke");
 
+const apiKey = "a1RWlmeEOslgJ8FjdmkFtA==w4Vcv7ujmmztRZmi";
+const options = {
+  method: "GET",
+  headers: {
+    "X-Api-Key": apiKey,
+  },
+};
+const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
+async function getJoke() {
+  const response = await fetch(apiURL, options);
+  const data = await response.json();
+  jokeEl.innerText = data[0].joke;
+}
+
+btnEl.addEventListener("click", getJoke);
