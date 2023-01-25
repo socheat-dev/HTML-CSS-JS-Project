386716f22c553e489622487e48bec566aad933d7
Socheat <hongsocheat38@gmail.com>
2023-01-25 08:49:09 +0700

    add some code in index.js

---
+++ FeedbackUI/index.js
@@ -0,0 +1,7 @@
+const ratingEls = document.querySelectorAll(".rating");
+
+ratingEls.forEach((ratingEl) => {
+  ratingEl.addEventListener("click", (event) => {
+    console.log(event.target.innerText || event.target.parentNode.innerText);
+  });
+});
